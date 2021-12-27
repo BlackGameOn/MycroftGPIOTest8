e96b68a4f52b399df535d87f87b65c40e403c6a5
BlackGameOn <67538261+BlackGameOn@users.noreply.github.com>
2021-12-27 15:42:37 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setup(14,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
 class LedControl(MycroftSkill):
     
@@ -15,6 +16,7 @@ class LedControl(MycroftSkill):
         self.speak_dialog('led_skill')
         GPIO.output(14,True)
         GPIO.output(15,True)
+        GPIO.output(18,True)
 
 def create_skill():
     return LedControl()
